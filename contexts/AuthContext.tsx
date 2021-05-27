f0fd3f443be49e59fde35e1f2bbba87152db212f
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-05-26 22:12:12 -0300

    Criando fluxo de autenticacao e passando so dados do usuario entre componentes

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import {useRouter} from 'next/router';
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -8,6 +15,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 }
 
@@ -18,7 +26,9 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({children}: AuthProviderProps) {
-  const isAuthenticated = false;
+  const router = useRouter();
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   async function signIn({email, password}: SignInCredentials) {
     try {
@@ -27,14 +37,22 @@ export function AuthProvider({children}: AuthProviderProps) {
         password,
       });
 
-      console.log(response.data);
+      const {permissions, roles} = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles
+      });
+
+      router.push('/dashboard');
     } catch(err) {
       console.log(err);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated}}>
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated}}>
       {children}
     </AuthContext.Provider>
   )
